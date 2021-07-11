9a834c6049b501d38602d050aa38e4f9b4d33f05
编程 <1660873558@qq.com>
2021-07-11 23:22:06 +0800

    二维数组的使用二

---
+++ src/ContactTest.java
@@ -0,0 +1,39 @@
+import javax.naming.PartialResultException;
+
+public class ContactTest {
+    public static void main(String[] args) {
+        System.out.println((float) 1.0 == (int)1);
+
+        int b[] ={1,3,5,7,9};
+        System.out.println(b[3]);
+        System.out.println(b[0]);
+        System.out.println(b[0]+b[3]);
+        System.out.println(b[0]+b[3]+b[1]+b[2]+b[4]);
+
+        int sum = 0;
+        for (int i = 0; i <= 4; i++){
+            sum = sum + b[i];
+        }
+        System.out.println(sum);
+
+        /*
+        for (int i = 1; i <= 100; i++ ){
+           if( i % 2 != 0)
+                System.out.println(i);
+            }
+
+       // int arr [] = new int[]
+
+        // arr = new
+
+
+        //int[] [] arr = new int[][]  {1,3,5,7,9}
+
+        //int scanner[] ;
+
+         */
+
+
+        }
+    }
+
